8f7d796e62fe40ca48023e3a837446f0dc131183
Alexander <strrife@gmail.com>
2015-12-16 01:23:15 +0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt-get update -y
+sudo apt-get install -y git
+sudo gem install sass
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm i -g grunt-cli bower forever
+sudo npm i -g node-sass
+cd /vagrant
+npm install
+bower install
---
+++ run.sh
@@ -0,0 +1,2 @@
+cd /vagrant/
+ENDPOINT="https://192.168.50.1:51007/" grunt serve > /dev/null 2>&1 &
